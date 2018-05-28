eea23a28f24efd20beb9e91df93271dfac773c52
Jose Martinez <deadworlds@me.com>
2018-05-28 02:48:43 -0700

    Display accelerometer data

--- Space Shooter/GameScene.swift
+++ Space Shooter/GameScene.swift
@@ -8,13 +8,38 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
-    
+    let motion = CMMotionManager()
+    var timer = Timer()
     
     override func didMove(to view: SKView) {
-        
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+            self.motion.startAccelerometerUpdates()
+        }
+            
+            /*
+            // Configure a timer to fetch the data.
+            self.timer = Timer(fire: Date(), interval: (1.0/2.0),
+                               repeats: true, block: { (timer) in
+                                // Get the accelerometer data.
+                                if let data = self.motion.accelerometerData {
+                                    let x = data.acceleration.x
+                                    let y = data.acceleration.y
+                                    let z = data.acceleration.z
+                                    
+                                    // Use the accelerometer data in your app.
+                                    
+                                    print("X: \(x), Y: \(y), Z: \(z)")
+                                }
+            })
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(self.timer, forMode: .defaultRunLoopMode)
+        } */
         
     }
     
@@ -47,5 +72,16 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if let data = self.motion.accelerometerData{
+            let x = data.acceleration.x
+            let y = data.acceleration.y
+            let z = data.acceleration.z
+            
+            print("X: \(x), Y: \(y), Z: \(z)")
+        }
+        
+        
+        
     }
 }
