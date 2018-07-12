b774ad0e2c6083fcc518ef752872e210d5405e91
Jose Martinez <deadworlds@me.com>
2018-07-11 19:40:08 -0700

    Began implementation of Enemy class.

--- Space Shooter/Enemy.swift
+++ Space Shooter/Enemy.swift
@@ -9,8 +9,42 @@
 import Foundation
 import SpriteKit
 
-class Enemy : Hazard {
+class Enemy : Hazard, HazardProtocol {
     
+    //Initilizae the object with a texture and a moving speed
+    init(texture:SKTexture, speed: CGFloat){
+     
+        super.init(texture: texture, hazardSpeed: speed)
+        self.setupPhysics()
+        
+    }
+    
+   
+    func setupPhysics() {
+        
+        if let texture = self.texture {
+            
+            self.physicsBody = SKPhysicsBody(rectangleOf: texture.size(), center: self.anchorPoint)
+            self.physicsBody?.affectedByGravity = false
+            self.physicsBody?.categoryBitMask = physicsCategories.hazard
+            self.physicsBody?.collisionBitMask = physicsCategories.none
+            self.physicsBody?.contactTestBitMask = physicsCategories.player | physicsCategories.playerWeapon | physicsCategories.bounds
+            
+        } else {
+            print("Unable to create Asteroid Physics body.")
+        }
+        
+    }
+    
+    
+    func updatePosition() {
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     
 }
