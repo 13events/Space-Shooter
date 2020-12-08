a1be1487057d92a46299f91eb695736d53abd186
13events <jdmartinez90@icloud.com>
2020-12-07 23:38:11 -0800

    Created Main Menu Scene * Created a Main Menu sks scene with a play button * Set MainMenu gamescene to load on app launch

---
+++ Space Shooter/MainMenuScene.swift
@@ -0,0 +1,31 @@
+//
+//  MainMenuScene.swift
+//  Space Shooter
+//
+//  Created by Jose Martinez on 12/7/20.
+//  Copyright © 2020 Jose Martinez. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene : SKScene {
+    
+    override func didMove(to view: SKView) {
+        let playButton = childNode(withName: "PlayButton") as? SKSpriteNode
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let locationInScene = touch!.location(in: self)
+        let touchedNode = self.atPoint(locationInScene)
+        let transition = SKTransition.fade(with: UIColor.black, duration: 0.5)
+        
+        if let nodeName = touchedNode.name {
+            if nodeName == "PlayButton" {
+               
+                let nextScene = SKScene(fileNamed: "GameScene")
+                self.view?.presentScene(nextScene!, transition: transition)
+            }
+        }
+    }
+}
